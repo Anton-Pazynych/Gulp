94376b4fb1fb696a14b561344883b400edd20447
Anton Pazynych <97320787+Anton-Pazynych@users.noreply.github.com>
2022-02-18 01:36:53 +0100

    Gulp / 1-s edit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Gulp pack   (18.02.2022)
+
+1. HTML include
+2. .scss to css
+3. img compress
+4. Browser-sync
---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+const gulp = require('gulp')
+const del = require('del')
+const dest = require('dest')
+const sass = require('gulp-sass')(require('sass'))
+const browsersync = require('browser-sync').create()
+const fileinclude = require('gulp-file-include')
+
+
+// const imagemin = require('gulp-imagemin')
+
+
+const paths = {
+  styles: {
+    src: 'src/styles/style.scss',
+    allsrc: 'src/styles/*.scss',
+    dest: 'dist/css/'
+  },
+  scripts: {
+    src: 'src/js/*.js',
+    dest: 'dist/js/'    
+  },
+  html: {
+    src: 'src/*.html',
+    allsrc: 'src/**/*.html',
+    dest: 'dist/'
+  },
+  img: {
+    src: 'src/img/*',
+    dest: 'dist/img/'
+  },
+}
+
+
+function clean() {
+  return del(['dist/*'])
+}
+
+function html() {
+  return gulp.src(paths.html.src)
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browsersync.stream())
+}
+
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass.sync({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(gulp.dest(paths.styles.dest)) 
+    .pipe(browsersync.stream())  
+}
+
+function scripts() {
+  return gulp.src(paths.scripts.src,)
+    .pipe(gulp.dest(paths.scripts.dest)) 
+    .pipe(browsersync.stream())
+}
+
+
+function img() {
+  return gulp.src(paths.img.src)
+    // .pipe(imagemin())
+    .pipe(gulp.dest(paths.img.dest))
+}
+
+
+
+function watch() {
+  browsersync.init({
+    server: {
+      baseDir: "./dist/"
+    }
+  }) 
+
+  gulp.watch(paths.html.allsrc).on('change', browsersync.reload)
+  gulp.watch(paths.html.allsrc, html)
+  gulp.watch(paths.styles.allsrc, styles)
+  gulp.watch(paths.scripts.src, scripts)
+  gulp.watch(paths.img.src, img)
+}
+
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts, html, img), watch)
+
+exports.clean = clean
+exports.html = html
+exports.styles = styles
+exports.scripts = scripts
+exports.img = img
+exports.watch = watch
+exports.build = build
+exports.default = build
